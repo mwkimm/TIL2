de4f68ffc34be22ba9f6a4a3c9693c3dfe916ca5
mwkimm <94061061+mwkimm@users.noreply.github.com>
2023-07-19 19:09:17 +0900

    230719

---
+++ app/src/main/java/net/flow9/thisiskotlin/kmwpractice/ClassPractice.kt
@@ -0,0 +1,26 @@
+//package net.flow9.thisiskotlin.kmwpractice
+//
+//
+//class Human (val name: String = "Anonoymous"){
+//
+//    constructor(name : String, age: Int)
+//
+//    init {
+//    println("New human has been born!!")
+//    }
+//
+//    fun eatingCake(){
+//        println("This is so yammmy~~~")
+//    }
+//}
+//
+//
+//fun main(){
+//    val human = Human( "minsoo")
+//
+//    val stranger = Human()
+//
+//    human.eatingCake()
+//
+//    println("this human's name is ${stranger.name}")
+//}
